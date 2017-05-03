e4dcd5b44a9bb1dc1948834971db35ce767caed2
parendu parikh <parendu parikh>
2017-05-02 23:34:46 -0400

    added pseudo code giftastic.js

---
+++ giftastic.js
@@ -0,0 +1,80 @@
+//Create array for animals
+
+var animals = ["Lion", "Tiger"];
+
+//create function to display GIF
+
+function animalGif() {
+//clear gifs-appear-here area
+
+//save input value 
+
+//create queryurl for ajax call with apiKey
+
+//Call ajax function
+
+//for each resonse value
+
+//create div class item
+
+// get rating
+
+// set varialbe p and display Rating on page
+
+//if rating is not r and pg-13 display GIFs
+
+
+//create img element
+
+//add class animal-image
+
+//get src url
+
+//set still url attr
+
+//set animate url attr 
+
+//set data-state attr 
+
+//prepend rating p to gifDiv
+
+//prepend animalImage to gifDiv
+
+
+};
+//Create function to render
+
+function displayButton() {
+
+    // Delete the content inside the animal-view div
+     $("#animal-view").empty();
+    //create button
+
+    var addButton = $("<button>");
+    //add class to button
+    
+   
+   //add text value to button
+    
+   
+    //append button
+    
+    //add attr to button data-name
+
+
+// on click function to get input value
+
+//set varialbe for input value
+
+
+//add input value to array
+
+
+//call displayButton function to render button
+ 
+//on click function for pause and play
+
+
+}:
+//clean up animal view and called displayButton call
+
